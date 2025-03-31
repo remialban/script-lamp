db5b4a70244bf94e8f08a0965069d6e302af2d53
Rémi ALBAN <59766320+remialban@users.noreply.github.com>
2025-03-31 11:52:36 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+MARIADB_PASSWORD=azerty
+
+apt-get update
+
+apt-get install -y libapache2-mod-php
+apt-get install -y mariadb-server
+apt-get install -y php-pdo php-pdo-mysql
+
+systemctl enable --now apache2
+systemctl enable --now mariadb
+
+mariadb -e "SET PASSWORD FOR root@localhost = PASSWORD('$MARIADB_PASSWORD')"
+
+
+a2enmod rewrite
+
+APACHE_CONF_FILE=/etc/apache2/sites-available/000-default.conf
+sed -i '/<\/VirtualHost>/d' $APACHE_CONF_FILE
+
+echo -e "\t<Directory /var/www/html>" >> $APACHE_CONF_FILE
+echo -e "\t\tAllowOverride All" >> $APACHE_CONF_FILE
+echo -e "\t</Directory>" >> $APACHE_CONF_FILE
+echo -e "\n</VirtualHost>" >> $APACHE_CONF_FILE
+
+
+systemctl restart apache2
+
+
+apt install phpmyadmin
+
+CONNECTED_USER=$(who | tr -s " " | cut -d " " -f1)
+
+chown -R $CONNECTED_USER:www-data /var/www/html
+sudo usermod -aG www-data $CONNECTED_USER
+
+chmod g+rxw /var/www/html
+chmod g+s /var/www/html
+
+PHP_VERSION=$(php --ini | grep etc | tail -n 1 | cut -d "/" -f4)
+
+PHP_FILE=/etc/php/${PHP_VERSION}/apache2/php.ini
+
+sed -i 's/display_errors =.*/display_errors=On/' "$PHP_FILE"
+
+systemctl restart apache2
+
+echo "Installation terminé!"
